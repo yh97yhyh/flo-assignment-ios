8aef5d1252e046780efe5fffd4fe1c82920c7826
영현 <yh97yhyh@naver.com>
2024-03-18 17:22:11 +0900

    first commit

---
+++ flo-swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  flo-swiftui
+//
+//  Created by 영현 on 3/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ flo-swiftui/flo_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  flo_swiftuiApp.swift
+//  flo-swiftui
+//
+//  Created by 영현 on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct flo_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
